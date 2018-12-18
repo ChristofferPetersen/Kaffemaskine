972f08b09612f8cbb1934de9af040a2488e26435
Christoffer Petersen <fantomlich.fujie@gmail.com>
2018-12-18 12:21:09 +0100

    Responsivt layout
    Index.php setup
    conn.php
    script.js
    style.css
    
    Der er lavet et helt layout klar til brug.

---
+++ htdocs/index.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="style.css">
+        <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script src="script.js"></script>
+    </head>
+    <body>
+        <div id="maskineBaggrund">
+            <div id="top">
+                <div id="maelkFloede">
+                    <h1>M</h1>
+                    <h1>Æ</h1>
+                    <h1>L</h1>
+                    <h1>K</h1>
+                </div>
+
+                <div id="sukker">
+                    <h1>S</h1>
+                    <h1>U</h1>
+                    <h1>K</h1>
+                    <h1>K</h1>
+                    <h1>E</h1>
+                    <h1>R</h1>
+                </div>
+
+                <div id="kakao">
+                    <h1>K</h1>
+                    <h1>A</h1>
+                    <h1>K</h1>
+                    <h1>A</h1>
+                    <h1>O</h1>
+                </div>
+
+                <div id="kaffe">
+                    <h1>K</h1>
+                    <h1>A</h1>
+                    <h1>F</h1>
+                    <h1>F</h1>
+                    <h1>E</h1>
+                </div>
+            </div>
+
+            <div id="bottom">
+                <div id="display">
+
+                </div>
+
+                <div id="indsats">
+                    <div id="kopHolder">
+
+                    </div>
+
+                    <div id="drypbakke">
+
+                    </div>
+                </div>
+
+                <div id="knapper">
+                    <div id="bryg">
+                        <button>Kaffe</button>
+                        <button>Kaffe med sukker</button>
+                        <button>Kaffe med mælk</button>
+                        <button>Espresso</button>
+                        <button>Mocca</button>
+                        <button>Cappuccino</button>
+                        <button>Cafelatte</button>
+                        <button>Wiener melange</button>
+                        <button>Kakao</button>
+                        <button>Varmt vand</button>
+                    </div>
+
+                    <div id="kontrol">
+                        <button>Styrke +</button>
+                        <button>Sukker +</button>
+                        <button>Mælk +</button>
+                        <button>Start bryg</button>
+                    </div>                    
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
