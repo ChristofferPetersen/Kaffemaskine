4672d44b2754465c39aa62b0ca43fdf838c139ca
Christoffer Petersen <fantomlich.fujie@gmail.com>
2018-12-19 12:28:53 +0100

    JQuery
    Tid + knap 11 - 14 færdig.
    Mangler sammenhæng med DB.

--- htdocs/script.js
+++ htdocs/script.js
@@ -1,62 +1,89 @@
 function Knaptryk() {
+    var tid = 0;
+
     $("button#knap1").click(function() {
+        tid = 10000
         $("h1#brygdisplay").show()
         $("h2#beskrivelsedisplay").show()
         $("h3#tiddisplay").show()
+        $("button#knap12").fadeOut()
+        $("button#knap13").fadeOut()
     })
 
     $("button#knap2").click(function() {
+        tid = 12000
         $("h1#brygdisplay").show()
         $("h2#beskrivelsedisplay").show()
         $("h3#tiddisplay").show()
+        $("button#knap13").fadeOut()
     })
 
     $("button#knap3").click(function() {
+        tid = 12000
         $("h1#brygdisplay").show()
         $("h2#beskrivelsedisplay").show()
         $("h3#tiddisplay").show()
+        $("button#knap12").fadeOut()
     })
 
     $("button#knap4").click(function() {
+        tid = 10000
         $("h1#brygdisplay").show()
         $("h2#beskrivelsedisplay").show()
         $("h3#tiddisplay").show()
+        $("button#knap12").fadeOut()
+        $("button#knap13").fadeOut()
     })
 
     $("button#knap5").click(function() {
+        tid = 15000
         $("h1#brygdisplay").show()
         $("h2#beskrivelsedisplay").show()
         $("h3#tiddisplay").show()
+        $("button#knap12").fadeOut()
+        $("button#knap13").fadeOut()
     })
 
     $("button#knap6").click(function() {
+        tid = 15000
         $("h1#brygdisplay").show()
         $("h2#beskrivelsedisplay").show()
         $("h3#tiddisplay").show()
+        $("button#knap12").fadeOut()
     })
 
     $("button#knap7").click(function() {
+        tid = 15000
         $("h1#brygdisplay").show()
         $("h2#beskrivelsedisplay").show()
         $("h3#tiddisplay").show()
+        $("button#knap12").fadeOut()
     })
 
     $("button#knap8").click(function() {
+        tid = 15000
         $("h1#brygdisplay").show()
         $("h2#beskrivelsedisplay").show()
         $("h3#tiddisplay").show()
     })
 
     $("button#knap9").click(function() {
+        tid = 10000
         $("h1#brygdisplay").show()
         $("h2#beskrivelsedisplay").show()
         $("h3#tiddisplay").show()
+        $("button#knap12").fadeOut()
+        $("button#knap13").fadeOut()
     })
 
     $("button#knap10").click(function() {
+        tid = 6000
         $("h1#brygdisplay").show()
         $("h2#beskrivelsedisplay").show()
         $("h3#tiddisplay").show()
+        $("button#knap11").fadeOut()
+        $("button#knap12").fadeOut()
+        $("button#knap13").fadeOut()
     })
 
     $("button#knap11").click(function() {
@@ -70,9 +97,17 @@ function Knaptryk() {
     $("button#knap13").click(function() {
         $("h3#maelkdisplay").show()
     })
-
-    $("button#knap14").click(function() {
         
+    $("button#knap14").click(function() {
+        $("h1#brygdisplay").hide(tid)
+        $("h2#beskrivelsedisplay").hide(tid)
+        $("h3#tiddisplay").hide(tid)
+        $("h3#styrkedisplay").hide(tid)
+        $("h3#sukkerdisplay").hide(tid)  
+        $("h3#maelkdisplay").hide(tid)
+        $("button#knap11").show(tid)
+        $("button#knap12").show(tid)
+        $("button#knap13").show(tid)     
     })
 }
 
