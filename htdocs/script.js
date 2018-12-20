c070986df0eaab412827d8ea18e1c99fce81180f
Christoffer Petersen <fantomlich.fujie@gmail.com>
2018-12-20 10:23:23 +0100

    StopUr
    Stopur der tæller bryggens bryggetid

--- htdocs/script.js
+++ htdocs/script.js
@@ -1,8 +1,10 @@
 function Knaptryk() {
     var tid = 0;
+    var stopUr = 0;
 
     $("button#knap1").click(function() {
         tid = 10000
+        stopUr = 10
         $("h3#brygdisplay").show().text("Kaffe.")
         $("p#beskrivelsedisplay").show().text("Sort kaffe uden sukker og mælk.")
         $("p#tiddisplay").show().text("10")
@@ -12,6 +14,7 @@ function Knaptryk() {
 
     $("button#knap2").click(function() {
         tid = 12000
+        stopUr = 12
         $("h3#brygdisplay").show().text("Kaffe med sukker.")
         $("p#beskrivelsedisplay").show().text("Sort kaffe med sukker.")
         $("p#tiddisplay").show().text("12")
@@ -20,6 +23,7 @@ function Knaptryk() {
 
     $("button#knap3").click(function() {
         tid = 12000
+        stopUr = 12
         $("h3#brygdisplay").show().text("Kaffe med mælk.")
         $("p#beskrivelsedisplay").show().text("Sort kaffe med mælk.")
         $("p#tiddisplay").show().text("12")
@@ -28,6 +32,7 @@ function Knaptryk() {
 
     $("button#knap4").click(function() {
         tid = 10000
+        stopUr = 10
         $("h3#brygdisplay").show().text("Espresso.")
         $("p#beskrivelsedisplay").show().text("Halv kop med ekstra stærk kaffe.")
         $("p#tiddisplay").show().text("10")
@@ -37,6 +42,7 @@ function Knaptryk() {
 
     $("button#knap5").click(function() {
         tid = 15000
+        stopUr = 15
         $("h3#brygdisplay").show().text("Mocca.")
         $("p#beskrivelsedisplay").show().text("Kvalitets kaffe fusioneret med kakao.")
         $("p#tiddisplay").show().text("15")
@@ -46,6 +52,7 @@ function Knaptryk() {
 
     $("button#knap6").click(function() {
         tid = 15000
+        stopUr = 15
         $("h3#brygdisplay").show().text("Cappuccino.")
         $("p#beskrivelsedisplay").show().text("Espresso med skummet varm mælk.")
         $("p#tiddisplay").show().text("15")
@@ -54,6 +61,7 @@ function Knaptryk() {
 
     $("button#knap7").click(function() {
         tid = 15000
+        stopUr = 15
         $("h3#brygdisplay").show().text("Cafelatte.")
         $("p#beskrivelsedisplay").show().text("Kaffe med 2/3 dele mælk.")
         $("p#tiddisplay").show().text("15")
@@ -62,6 +70,7 @@ function Knaptryk() {
 
     $("button#knap8").click(function() {
         tid = 15000
+        stopUr = 15
         $("h3#brygdisplay").show().text("Wiener melange.")
         $("p#beskrivelsedisplay").show().text("En herlig blanding af kaffe med mælk, sukker og blandet med kakao.")
         $("p#tiddisplay").show().text("15")
@@ -69,6 +78,7 @@ function Knaptryk() {
 
     $("button#knap9").click(function() {
         tid = 10000
+        stopUr = 10
         $("h3#brygdisplay").show().text("Kakao.")
         $("p#beskrivelsedisplay").show().text("Varm kakao baseret på mælk.")
         $("p#tiddisplay").show().text("10")
@@ -78,6 +88,7 @@ function Knaptryk() {
 
     $("button#knap10").click(function() {
         tid = 6000
+        stopUr = 6
         $("h3#brygdisplay").show().text("Varmt vand.")
         $("p#beskrivelsedisplay").show().text("Kogende vand 99 grader... Hot, hot & hot.")
         $("p#tiddisplay").show().text("6")
@@ -99,15 +110,30 @@ function Knaptryk() {
     })
         
     $("button#knap14").click(function() {
-        $("h3#brygdisplay").delay(tid).fadeOut(0)
-        $("p#beskrivelsedisplay").delay(tid).fadeOut(0)
-        $("p#tiddisplay").delay(tid).fadeOut(0)
-        $("p#styrkedisplay").delay(tid).fadeOut(0)
-        $("p#sukkerdisplay").delay(tid).fadeOut(0)
-        $("p#maelkdisplay").delay(tid).fadeOut(0)
-        $("button#knap11").delay(tid).fadeIn(0)
-        $("button#knap12").delay(tid).fadeIn(0)
-        $("button#knap13").delay(tid).fadeIn(0)  
+
+        var timer = setInterval(function() {
+            if(stopUr > 0)
+            {
+                stopUr -= 1
+                $("p#tiddisplay").text(stopUr)
+            }
+            else
+            {                
+                clearInterval(timer)
+            }
+        }
+        ,1000
+        )
+
+        $("h3#brygdisplay").delay(tid).hide(0)
+        $("p#beskrivelsedisplay").delay(tid).hide(0)
+        $("p#tiddisplay").delay(tid).hide(0)
+        $("p#styrkedisplay").delay(tid).hide(0)
+        $("p#sukkerdisplay").delay(tid).hide(0)
+        $("p#maelkdisplay").delay(tid).hide(0)
+        $("button#knap11").delay(tid).show(0)
+        $("button#knap12").delay(tid).show(0)
+        $("button#knap13").delay(tid).show(0)
     })
 }
 
